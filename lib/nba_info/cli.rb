81b789488eab1c102017a8ebc2b5f06863408401
RSchaubeck <richard.schaubeck@gmail.com>
2019-01-09 18:52:57 -0500

    added welcome message and input instructions and also method for scraping teams names and position in standings

--- lib/nba_info/cli.rb
+++ lib/nba_info/cli.rb
@@ -1,7 +1,14 @@
 class NbaInfo::CLI
 
   def call
-    puts "testing"
+    puts <<-DOC.gsub /^\s*/, ''
+      Welcome to the NBA Info CLI gem!
+      List of commands:
+      schedule - shows todays schedule
+      standings - shows current standings
+      Lastly, you can type the name of the team (omit the city e.g. knicks, warriors)
+      to get more info about them
+    DOC
   end
 
 end
